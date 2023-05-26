0c2bbcb156dbeaceafbee9d7a6e9ecb57b3f0bfc
Aiswarya-KP <aiswaryakp2k@gmail.com>
2023-05-26 17:21:44 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { CarouselModule } from './carousel/carousel.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+    //CarouselComponent
+  imports: [BrowserModule,AppRoutingModule,CarouselModule],
   providers: [],
   bootstrap: [AppComponent]
 })
